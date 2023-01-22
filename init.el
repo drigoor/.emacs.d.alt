a1f512b21f0163a448a50b17929c9d59cfccdb85
drigoor <>
2023-01-22 16:42:28 +0000

    add git grep improvements

--- init.el
+++ init.el
@@ -257,6 +257,54 @@
         uniquify-after-kill-buffer-p t ; rename after killing uniquified
         uniquify-ignore-buffers-re "^\\*")) ; don't muck with special buffers
 
+(use-package grep
+  :ensure nil
+  :bind
+  (("C-S-s" . extra-gitgrep)
+   ("C-M-S-s" . extra-gitgrep-with-comments))
+  :config
+    (defvar extra-gitgrep-default-git-repo nil
+    "If a git repository is not found, this specifies where to search by default.")
+
+  (defvar extra-gitgrep-file-extensions "*.lisp *.cl"
+    "Default file extensions to search.")
+
+  (defvar extra-gitgrep-default-comment-string ";"
+    "Default string for comments.")
+
+  (defun extra-gitgrep-command (search-in-comments-p)
+    (let ((dir (or (vc-root-dir)
+                   extra-gitgrep-default-git-repo
+                   default-directory)))
+      (let ((regexp (grep-read-regexp)))
+        (when (and (stringp regexp) (> (length regexp) 0))
+          (let ((command (grep-expand-template "git --no-pager grep -n -i -e <R> -- <F>"
+                                               (if search-in-comments-p
+                                                   regexp
+                                                 (format "^[^%s]*%s" extra-gitgrep-default-comment-string regexp))
+                                               extra-gitgrep-file-extensions)))
+            (when command
+              (add-to-history 'grep-history command)
+              (let ((default-directory dir)
+                    (compilation-environment (cons "PAGER=" compilation-environment)))
+                ;; Setting process-setup-function makes exit-message-function work
+                ;; even when async processes aren't supported.
+                (compilation-start command 'grep-mode))
+              (when (eq next-error-last-buffer (current-buffer))
+                (setq default-directory dir))))))))
+
+  (defun extra-gitgrep ()
+    (interactive)
+    (extra-gitgrep-command nil)
+    (select-window (get-buffer-window "*grep*")))
+
+  (defun extra-gitgrep-with-comments ()
+    (interactive)
+    (extra-gitgrep-command t)
+    (select-window (get-buffer-window "*grep*")))
+
+  (setq extra-gitgrep-file-extensions "*.lisp *.cl"))
+
 ;; -----------------------------------------------------------------------------
 
 (use-package expand-region ; from: https://susamn.medium.com/ultimate-emacs-setup-with-documentation-in-org-mode-8ed32e2b3487
